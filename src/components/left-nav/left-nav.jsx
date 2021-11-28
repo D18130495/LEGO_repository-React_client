7e3849359d16a30f97a1f861d6ace1e6b3a8ef9b
D18130495 <D18130495@mytudublin.ie>
2021-11-28 02:20:47 +0000

    update auth function with left-nav display

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -14,13 +14,15 @@ import {
     UserOutlined,
     GithubOutlined,
   } from '@ant-design/icons';
+  import memoryUser from '../../utils/memoryUser'
 
   const { SubMenu } = Menu
 
 class LeftNav extends Component {
     render() {
+        const menus = memoryUser.user.data.menus // get current user, use to ge menus and display the function
 
-        var path = this.props.location.pathname
+        var path = this.props.location.pathname // use to show the second path
 
         if(path.indexOf('/home') === 0)
         {
@@ -40,36 +42,48 @@ class LeftNav extends Component {
                 </header>
 
                 <Menu mode="inline" theme="light" selectedKeys={path} defaultOpenKeys={['sub1', 'sub2']}>
-                    <Menu.Item key="/home" icon={<AppstoreOutlined />}>
-                        <Link to='/home'>
-                            Home
-                        </Link>
-                    </Menu.Item>
+                    { menus.map((index) => (!index.indexOf('/home')?
+                        <Menu.Item key="/home" icon={<AppstoreOutlined />}>
+                            <Link to='/home'>
+                                Home
+                            </Link>
+                        </Menu.Item> : null))
+                    }
 
                     <SubMenu key="sub1" icon={<ClusterOutlined />} title="Manage LEGO sets">
-                        <Menu.Item key="/theme" icon={<ApartmentOutlined />}>
-                            <Link to='/theme'>
-                                Add theme
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="/setInfo" icon={<AppstoreAddOutlined />}>
-                            <Link to='/setInfo'>
-                                Add set information
-                            </Link>
-                        </Menu.Item>
-                    </SubMenu>
+                        { menus.map((index) => (!index.indexOf('/manageSet/theme')? 
+                            <Menu.Item key="/theme" icon={<ApartmentOutlined />}>
+                                <Link to='/theme'>
+                                    Add theme
+                                </Link>
+                            </Menu.Item> : null))
+                        }
 
-                    <SubMenu key="sub2" icon={<UserOutlined />} title="Manage user">
-                        <Menu.Item key="/user" icon={<UserAddOutlined />}>
-                            <Link to='/user'>
-                                Add user
-                            </Link>
-                        </Menu.Item>
+                        { menus.map((index) => (!index.indexOf('/manageSet/setinfo')? 
+                            <Menu.Item key="/setInfo" icon={<AppstoreAddOutlined />}>
+                                <Link to='/setInfo'>
+                                    Add set information
+                                </Link>
+                            </Menu.Item> : null))
+                        }
                     </SubMenu>
+                    
+                    { menus.map((index) => (index.indexOf('/manageUser/user')? null :
+                        <SubMenu key="sub2" icon={<UserOutlined />} title="Manage user">
+                            <Menu.Item key="/user" icon={<UserAddOutlined />}>
+                                <Link to='/user'>
+                                    Add user
+                                </Link>
+                            </Menu.Item>
+                        </SubMenu>))
+                    }
+                        
 
-                    <Menu.Item key="/github" icon={<GithubOutlined />}>
-                        <a href="https://github.com/D18130495/LEGO_repository-React_client" src="github" target='blank'><span>GitHub</span></a>        
-                    </Menu.Item>
+                    { menus.map((index) => (index.indexOf('/github')? null :
+                        <Menu.Item key="/github" icon={<GithubOutlined />}>
+                            <a href="https://github.com/D18130495/LEGO_repository-React_client" src="github" target='blank'><span>GitHub</span></a>        
+                        </Menu.Item>))
+                    }
                 </Menu>
             </div>
         )
